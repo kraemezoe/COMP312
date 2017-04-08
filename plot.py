bba8510cbf22475f5a9e0d5682f0ce77209c5a36
haslamed <haslamenator@gmail.com>
2017-04-08 18:31:11 +1200

    Modified plot.py to print a usage string if the user doesn't give enough arguments Added code to be compatible with data files recorded on windows; removing \r from the end of the recorded strings.

--- plot.py
+++ plot.py
@@ -10,17 +10,25 @@ import numpy as np
 from numpy import ma
 import matplotlib.pyplot as plt
 
+# if there aren't enough arguments
+if len(sys.argv)<2:
+	print "Usage: python %s <data filename>" % sys.argv[0]
+	sys.exit()
+
 # read data
 fn = sys.argv[1]
 fh = open(sys.argv[1])
 fd = fh.read().split('\n')[4:-1]
 fd = [x.split('  ') for x in fd]
 
+# in case this file was recorded on a windows machine, we need to strip all \r
+fd = [[x[0],x[1].split('\r')[0]] for x in fd]
+print fd
 
 # net customers in system
 customers = 0
-time = []
-net_customers = []
+time = [float(fd[0][0])]
+net_customers = [0]
 for rec in fd:
 	if rec[1] == 'a':
 		customers += 1
@@ -39,15 +47,38 @@ for rec in fd:
 
 # interarrival time
 arrivals = filter(lambda x: x[1]=='a',fd)
-interarrivals = 
+interarrivals = \
 	[float(i[0])-float(j[0]) for i,j in zip(arrivals,[fd[4]]+arrivals[:-1])]
 interarrival = sum(interarrivals)/len(interarrivals)
 
 # service time - harder to do
+stime = []
+lastc = 0
+last_time = 0
+for c,t in zip(net_customers,time):
+	if c-lastc<0:
+		if last_time!=0:
+			if c>0:
+				stime.append((t-last_time)/2)
+			else:
+				stime.append(t-last_time)	
+		last_time = t
+	
+	lastc = c
+stime = sum(stime)/len(stime)
 
 # print stats
-print "Interarrival time: %f seconds, I presume" % interarrival
+print "Interarrival time: %f seconds" % interarrival
+print "Interarrival rate: %f per minute" % (interarrival**-1*60)
+print "Service time: %f seconds" % stime
+print "Service rate: %f per minute" % (stime**-1*60)
+print "C*Service rate: %f per minute" % (stime**-1*60*2)
+print "Converges? %s" % ('yes' if (interarrival**-1*60)/(stime**-1*60*2)<1 else 'false')
+
+
+# change times to 24 hour
+time = [x/60/60 for x in time]
 
 # show graph		
-plt.step(np.array(time),np.array(net_customers))
+plt.step(time,net_customers)
 plt.show()
